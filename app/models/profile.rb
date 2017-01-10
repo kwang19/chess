ab4316e7462a3369544d29beac8b1820a5349249
Raghu Betina <raghu@firstdraft.com>
2017-01-10 20:58:54 +0000

    Direct association between profiles and profiledetails

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,10 @@
 class Profile < ApplicationRecord
   # Direct associations
 
+  has_many   :profiledetails,
+             :foreign_key => "profiles_id",
+             :dependent => :destroy
+
   belongs_to :users,
              :class_name => "Chessplayer",
              :foreign_key => "chessplayer_id"
--- app/models/profiledetail.rb
+++ app/models/profiledetail.rb
@@ -1,6 +1,8 @@
 class Profiledetail < ApplicationRecord
   # Direct associations
 
+  belongs_to :profiles
+
   # Indirect associations
 
   # Validations
